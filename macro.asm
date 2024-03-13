38f8984800d79e581a54b9c79d7843fb221f2b39
zxc_ghoul <AlexisV.Shishkin@gmail.com>
2024-03-14 03:55:07 +0700

    fixes:

---
+++ macro.asm
@@ -0,0 +1,17 @@
+.macro syscall %n
+    li a7, %n
+    ecall
+.end_macro
+
+.macro readch
+    syscall 12
+.end_macro
+
+.macro printch
+    syscall 11
+.end_macro
+
+.macro exit %ecode
+    li a0, %ecode
+    syscall 93
+.end_macro
